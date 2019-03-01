290064188a28fe41047dd172e8d353dc93969456
scream <hou.jennifer35@gmail.com>
2019-02-28 20:14:26 -0500

    modified api routes adding where clause for sequelize

--- public/js/app.js
+++ public/js/app.js
@@ -3,50 +3,34 @@
  */
 
 const registerUser = function (event){ 
-    event.preventDefault();
-    const username = $('#reg-username').val().trim();
-    const password = $('#reg-password').val().trim();
-  
-    $.post('/api/user',{email: username, password: password})
-    .then(function(data){
-      console.log(data);
-    })
-  }
-  
-  $('#register-btn').on('click', registerUser);
-  
-  /**
-   * Login Users
-   */
-  
+  event.preventDefault();
+  const username = $('#reg-username').val().trim();
+  const password = $('#reg-password').val().trim();
 
-  let usersArray = [];
-
-  const loginUser = function (event){ 
-    event.preventDefault();
-
-    const username = $('#login-username').val().trim();
-    const password = $('#login-password').val().trim();
-
-    $.get('/api/users').then(function(data) {
-      console.log(data);
-      for(i = 0; i < data.length; i++) {
-        usersArray.push(data[i])
-      }
-    }).then(function(){
-    for(i = 0;i < usersArray.length;i++) {
-      if(username === usersArray[i].email && password === usersArray[i].password) {
-        $.post('/api/session',{email: username, password: password})
-        .then(function(data){
-            sessionStorage.setItem('token', data.id)
-            window.location.href = "post.html";
-            return
-      })
-    }else{
-     console.log("Incorrect Account and Password")
-    }
-  }
- })
+  $.post('/api/user',{email: username, password: password})
+  .then(function(data){
+    console.log(data);
+  })
 }
-  
-  $('#login-btn').on('click', loginUser);
+
+$('#register-btn').on('click', registerUser);
+
+/**
+ * Login Users
+ */
+
+const loginUser = function (event){ 
+  event.preventDefault();
+  const email = $('#login-username').val().trim();
+  const password = $('#login-password').val().trim();
+
+  $.post('/api/session', {email: email, password: password})
+  .then(function(data){
+    // console.log(data);
+    if(data){
+      sessionStorage.setItem('token', data.id)
+      window.location.href = "post.html";
+    }
+  })
+}
+$('#login-btn').on('click', loginUser);
