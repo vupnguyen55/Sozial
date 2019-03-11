e7f0dc68f9aeb60ae11e6ade4f0c341d45c581a1
HTwa <turner1093@gmail.com>
2019-03-11 12:53:32 -0400

    profile picture working code

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Profile = (props) => (
-    <div>
-        <img className='profile-pic' src='https://cdn.pixabay.com/photo/2015/12/08/01/04/deer-1082318__340.jpg' alt='profile'></img>
-        
-        <p>{props.fullname}</p>
-    </div>
+const Profile = props => (
+  <div>
+    <img className="profile-pic" src={props.picture} alt="" />
+
+    <p>{props.fullname}</p>
+  </div>
 );
 
 export default Profile;
- 
