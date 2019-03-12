37ae8b3cefed37c3654c4e9f172308ef7ef949ce
vupnguyen55 <vupnguyen55@gmail.com>
2019-03-12 12:12:03 -0400

    pulled Harrisons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,13 +5,8 @@ const Home = (props) => (
     
         <div key={note.id} className="card">
             <div className="card-body">
-<<<<<<< HEAD
-                <h5 className="card-subtitle mb-2 text-muted">{note.title} {note.UserId}</h5>
-                 <span className="card-text">{note.body}</span>
-=======
                 <h5 className="card-subtitle mb-2 text-muted">{note.title} {note.User.full_name}</h5>
                 <p className="card-text">{note.body}</p>
->>>>>>> 2c8ba86b24c04fc8b7f515376ed72f8082ee76bb
                 <hr></hr>
                 <div className="btn-group" role="group">
                     <button className="btn btn-link btn-sm">
