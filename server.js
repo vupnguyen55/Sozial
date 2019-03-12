2b8b59f7e389da7d9786cc5f8b610a40cb7e7bbc
scream <hou.jennifer35@gmail.com>
2019-03-12 15:08:35 -0400

    bugs fixes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Starts our server.
-db.sequelize.sync().then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
   });
